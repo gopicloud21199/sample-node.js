e0ed4eb39e716e45fec424f34b44c23d0591baea
gopicloud21199 <gopinatha@cloudthat.com>
2024-11-20 19:57:51 +0530

    Update Checkout.groovy

--- shared-library/vars/Checkout.groovy
+++ shared-library/vars/Checkout.groovy
@@ -2,7 +2,7 @@ def call() {
     stage('Checkout Code') {
         steps {
             // Get the latest code from your version control system
-            checkout scm
+            git branch: 'main', url: 'https://github.com/gopicloud21199/sample-node.js.git'
         }
     }
 }
